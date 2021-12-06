64834fa10b7c0eb9a5591eb25366a0b0930c3a83
Lucas Armstrong <17970245+LucasArmstrong@users.noreply.github.com>
2021-12-06 16:31:56 -0500

    removing unused code

--- 2-ValidateSubsequence.js
+++ 2-ValidateSubsequence.js
@@ -1,11 +1,9 @@
 // determine if sequence array is a subsequence of the first array
 function isValidSubsequence(array, sequence) {
 	var y = 0;
-	var s = [];
 	var num;
 	while (num = array.shift()) {
 		if (num === sequence[y]) {
-			s.push(num);
 			if (y === sequence.length-1) return true;
 			y++;
 		}
