8af03f2737c3bd3e83ea76b52ed33c0a1117709c
Lucas Armstrong <17970245+LucasArmstrong@users.noreply.github.com>
2021-12-13 12:09:41 -0500

    query min wait times

---
+++ 8-MinimumWaitingTime.js
@@ -0,0 +1,34 @@
+// given array of query execution times, output the minimum execution time for executing all queries
+function minimumWaitingTime(queries) {
+	let time = 0;
+	if (queries.length <= 1) {
+		return time;
+	}
+	queries.sort((a,b) => a-b);
+	
+	for (let i = 0; i < queries.length; i++) {
+		let elementsRemain = queries.length - (i + 1);
+		time += queries[i] * elementsRemain;
+	}
+	
+	return time;
+}
+
+/**
+// with a reducer 
+function minimumWaitingTime(queries) {
+	let time = 0;
+	if (queries.length <= 1) {
+		return time;
+	}
+	queries.sort((a,b) => a-b);
+	
+	const reducer = (previousValue, currentValue) => previousValue + currentValue;
+	for (let i = 1; i < queries.length; i++) {
+		time += queries.slice(0, i).reduce(reducer);
+	}
+	
+	return time;
+}
+
+*/
